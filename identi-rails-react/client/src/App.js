1c3ef0d59e17e21e40f012608ad7d1bdb6f0b9a2
paulpellom@gmail.com <paulpellom@gmail.com>
2018-04-11 10:30:48 -0400

    footer init build: bg color

--- identi-rails-react/client/src/App.js
+++ identi-rails-react/client/src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
 import LandingPage from './components/LandingPage'
-
+import Footer from './components/Footer'
 class App extends Component {
   render() {
     return (
       <div className="App">
       <NavBar />
       <LandingPage />
-       
+      <Footer />
       </div>
     );
   }
